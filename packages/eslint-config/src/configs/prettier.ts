5f1978ade988e8859215ac9640e0ec7bbaaa263a
f3ve <free.eve@pm.me>
2024-02-27 10:30:35 -0900

    fix(eslint-config): added type to FlatEslintConfigItem import

--- packages/eslint-config/src/configs/prettier.ts
+++ packages/eslint-config/src/configs/prettier.ts
@@ -1,4 +1,4 @@
-import { FlatESLintConfigItem } from 'eslint-define-config';
+import { type FlatESLintConfigItem } from 'eslint-define-config';
 import { GLOB_DEFAULT_FILES } from '../globs';
 import prettierPlugin from 'eslint-plugin-prettier';
 import eslintConfigPrettier from 'eslint-config-prettier';
